d11907acf6f02598a76969bc437fafd51469589e
Tống Văn Đức <tongduc315@gmail.com>
2024-06-05 22:28:43 +0700

    Mua hàng

--- src/Controllers/Client/ProductController.php
+++ src/Controllers/Client/ProductController.php
@@ -3,14 +3,26 @@
 namespace Ductong\XuongOop\Controllers\Client;
 
 use Ductong\XuongOop\Commons\Controller;
+use Ductong\XuongOop\Models\Product;
 
 class ProductController extends Controller
 {
+    private Product $product;
+
+    public function __construct()
+    {
+        $this->product = new Product();
+    }
+    
     public function index() {
         echo __CLASS__ . '@' . __FUNCTION__;
     }
 
     public function detail($id) {
-        echo __CLASS__ . '@' . __FUNCTION__ . '@' . $id;
+        $product = $this->product->findByID($id);
+
+        $this->renderViewClient('product-detail', [
+            'product' => $product
+        ]);
     }
 }
