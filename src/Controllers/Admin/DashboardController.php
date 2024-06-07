dbc2e325e12ca5443f661b79b33e22af195a6b9e
Tống Văn Đức <tongduc315@gmail.com>
2024-06-07 09:17:25 +0700

    Bổ sung views vào product và làm biểu đồ

--- src/Controllers/Admin/DashboardController.php
+++ src/Controllers/Admin/DashboardController.php
@@ -4,10 +4,31 @@ namespace Ductong\XuongOop\Controllers\Admin;
 
 use Ductong\XuongOop\Commons\Controller;
 use Ductong\XuongOop\Commons\Helper;
+use Ductong\XuongOop\Models\Product;
 
 class DashboardController extends Controller
 {
-    public function dashboard() {        
-        $this->renderViewAdmin(__FUNCTION__);
+    private Product $product;
+
+    public function __construct()
+    {
+        $this->product = new Product();
+    }
+
+    public function dashboard() {    
+        $products = $this->product->all();
+
+        $analysisProduct = array_map(function ($item) {
+            return [
+                $item['name'],
+                $item['views']
+            ];
+        }, $products);
+
+        array_unshift($analysisProduct, ['Tên sản phẩm', 'Lượt views']);
+
+        $this->renderViewAdmin(__FUNCTION__, [
+            'analysisProduct' => $analysisProduct
+        ]);
     }
 }
